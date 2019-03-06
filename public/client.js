9caf267200fea4f5e192932467b4272062f07129
Glitch (wary-origami) <none>
2019-03-06 18:03:53 +0000

    🎻🍄 Checkpoint ./views/index.html:3109124/1901 ./public/style.css:3109124/538 ./public/client.js:3109124/1840

--- public/client.js
+++ public/client.js
@@ -34,6 +34,41 @@
   })  
   
   
+  
+//start scrollmagic simle demo 
+  
+  var controller = new ScrollMagic.Controller();
+  
+  // Parallax background
+new ScrollMagic.Scene({
+        triggerElement: "#parallax",
+        triggerHook: "onEnter",
+    })
+    .duration('200%')
+    .setTween("#parallax", {
+        backgroundPosition: "50% 100%",
+        ease: Linear.easeNone
+    })
+    //.addIndicators() // for debugging purposes
+    .addTo(controller);
+	
+new ScrollMagic.Scene({
+        triggerElement: "#slidein",
+        triggerHook: "onLeave",
+    })
+    .setPin("#slidein")
+    //.addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+ 
+new ScrollMagic.Scene({
+        triggerElement: "#slidein2",
+        triggerHook: "onLeave",
+    })
+    .setPin("#slidein2")
+    //.addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+  
+  /*
     var wh = window.innerHeight,
 	$iphone = $('.iphone'),
 	$innerS1 = $('.innerS1'),
@@ -122,6 +157,8 @@
   })
   .setTween(iphoneContent2Tl)
   .addTo(ctrl);
+  
+  */
 
 console.log('woot');
   })();
